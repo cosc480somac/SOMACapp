d8de0e5ea90bfc34d363d8ac14cf878059d7fdf4
Chris Weber <cwebftw@gmail.com>
2013-03-24 20:17:40 -0400

    Created app, added cucumber tests..

---
+++ Scheduler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduler::Application.config.secret_token = 'db939cf6adef881a64c2302fd51d6d787f3fa9f404a019a6fa8bfca7b17030204be360509415e676dd192934be300877c02945a09b98c57461580157f9a8697d'
